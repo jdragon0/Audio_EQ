94132f8da0364269aaffd6985a2335d021852843
jdragon0 <jdragon0@naver.com>
2024-09-15 02:04:35 +0900

    Create test.py

---
+++ test.py
@@ -0,0 +1,27 @@
+import BiquadFilter as filter
+import FilterPlot
+import numpy as np
+import scipy.signal as sc
+import matplotlib.pyplot as plt
+
+fs = 48000
+fc = np.array([100,200,400,800])
+gain = np.array([-10,10,10,10])
+Q = np.array([1,1,1,1])
+filterType = np.array(["peak","peak","peak","peak"])
+totalGain = 10
+
+myfilter = filter.BiquadEQ(fs,fc,gain,Q,filterType,totalGain)
+sos1 = myfilter.getSOS()
+
+fs = 48000
+fc = np.array([50,100,200,400,800,1600])
+gain = np.array([10,-10,10,7,4,4])
+Q = np.array([1,1,1,1,1,1])
+filterType = np.array(["peak","peak","peak","peak","peak","peak"])
+totalGain = 10
+
+myfilter.reset(fs,fc,gain,Q,filterType,totalGain)
+sos2 = myfilter.getSOS()
+
+FilterPlot.sosBodePlot(fs,1,24000,2**10,sos1,sos2)
